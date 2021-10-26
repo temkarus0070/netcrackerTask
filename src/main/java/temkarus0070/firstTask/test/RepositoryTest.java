6b77b4eb39b78e0a3e46faef4ccdd01e8fff1349
Artyom Pretkov <pretkovartyom@gmail.com>
2021-10-26 20:56:12 +0300

    add test

---
+++ src/main/java/temkarus0070/firstTask/test/RepositoryTest.java
@@ -0,0 +1,76 @@
+package temkarus0070.firstTask.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+import temkarus0070.firstTask.models.contract.Contract;
+import temkarus0070.firstTask.models.contract.DigitalTelevisionContract;
+import temkarus0070.firstTask.models.contract.MobileConnectionContract;
+import temkarus0070.firstTask.models.contract.WireInternetContract;
+import temkarus0070.firstTask.repository.ArrayListImpl;
+import temkarus0070.firstTask.repository.Repository;
+import temkarus0070.firstTask.repository.RepositoryImpl;
+
+import java.util.*;
+
+public class RepositoryTest {
+    Random random=new Random(1999);
+    @Test
+    public void testAdding(){
+        Long num= random.nextLong();
+        RepositoryImpl repository=new RepositoryImpl();
+        Contract contract=new DigitalTelevisionContract();
+        contract.setId(num);
+        repository.add(contract);
+        Assert.assertSame(repository.get(num).get(),contract);
+        repository.remove(num);
+    }
+
+    @Test
+    public void testRemove(){
+        int num= random.nextInt(3000);
+        RepositoryImpl repository=new RepositoryImpl();
+        Contract contract=new DigitalTelevisionContract();
+        contract.setId(num);
+        repository.add(contract);
+        repository.remove((long)num);
+        Assert.assertTrue(repository.get((long)num).isEmpty());
+    }
+
+    @Test
+    public void addManyContracts(){
+        int n= random.nextInt(10000);
+        RepositoryImpl repository=new RepositoryImpl();
+        Collection<Contract> contractCollection=new ArrayListImpl();
+        for(int i=0;i<n;i++){
+            int choice= random.nextInt(3);
+            Contract contract=null;
+            switch (choice){
+                case 0:
+                    contract=new DigitalTelevisionContract();
+                    contract.setId(random.nextInt(1000000));
+                    contractCollection.add(contract);
+                    break;
+                case 1:
+                    contract=new MobileConnectionContract();
+                    contract.setId(random.nextInt(1000000));
+                    contractCollection.add(contract);
+                    break;
+                case 2:
+                    contract=new WireInternetContract();
+                    contract.setId(random.nextInt(1000000));
+                    contractCollection.add(contract);
+                    break;
+
+            }
+            repository.add(contract);
+
+        }
+
+        for (Contract contract:contractCollection){
+            Assert.assertTrue(repository.get(contract.getId()).isPresent());
+        }
+
+    }
+}
+
