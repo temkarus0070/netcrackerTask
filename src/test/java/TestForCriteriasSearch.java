03381ae0b24367d473534de96bf187c4dbf2d513
artyom <pretkovartyom@gmail.com>
2021-11-09 22:39:41 +0300

    repair tests for sort and search function

--- src/test/java/TestForCriteriasSearch.java
+++ src/test/java/TestForCriteriasSearch.java
@@ -12,29 +12,40 @@ import java.sql.Date;
 
 public class TestForCriteriasSearch {
     @Test
-    public void testSearchByAge(){
-        ContractRepositoryImpl contractRepository=new ContractRepositoryImpl();
-        Person pupkin=new Person(1,"Vasya","Pupkin","Ivanovich", Gender.MALE,111,1111, java.sql.Date.valueOf("2000-04-09"));
-        MobileConnectionContract mobileConnectionContract=new MobileConnectionContract(444,0,88,1, java.sql.Date.valueOf("2020-03-09"), Date.valueOf("2023-04-09"),
-                77,pupkin);
-        DigitalTelevisionContract digitalTelevisionContract=new DigitalTelevisionContract("big",2, java.sql.Date.valueOf("2020-04-09"), java.sql.Date.valueOf("2022-04-09"),333,pupkin);
-        Person notPupkin=new Person(2,"Elizabeth","Peterson","Ivanovna",Gender.FEMALE,888,2222,Date.valueOf("1980-04-09"));
-        WireInternetContract wireInternetContract=new WireInternetContract(100,4,Date.valueOf("2018-03-09"),Date.valueOf("2024-08-09"),444,notPupkin);
-        contractRepository.add(mobileConnectionContract,digitalTelevisionContract,wireInternetContract);
-        Assert.assertEquals(contractRepository.getByCriterias(e->e.getContractOwner().getAge()>25).stream().findFirst().get(),wireInternetContract);
+    public void testSearchByAge() {
+        ContractRepositoryImpl contractRepository = new ContractRepositoryImpl();
+        Person pupkin = new Person(1, "Vasya", "Pupkin", "Ivanovich", Gender.MALE, 111, 1111, java.sql.Date.valueOf("2000-04-09"));
+        MobileConnectionContract mobileConnectionContract = new MobileConnectionContract(444, 0, 88, 1, java.sql.Date.valueOf("2020-03-09"), Date.valueOf("2023-04-09"),
+                77, pupkin);
+        DigitalTelevisionContract digitalTelevisionContract = new DigitalTelevisionContract("big", 2, java.sql.Date.valueOf("2020-04-09"), java.sql.Date.valueOf("2022-04-09"), 333, pupkin);
+        Person notPupkin = new Person(2, "Elizabeth", "Peterson", "Ivanovna", Gender.FEMALE, 888, 2222, Date.valueOf("1980-04-09"));
+        WireInternetContract wireInternetContract = new WireInternetContract(100, 4, Date.valueOf("2018-03-09"), Date.valueOf("2024-08-09"), 444, notPupkin);
+        contractRepository.add(mobileConnectionContract, digitalTelevisionContract, wireInternetContract);
+
+        ContractRepositoryImpl contractRepositoryWithElders = contractRepository.getByCriterias(e -> e.getContractOwner().getAge() > 25);
+        Contract contract = contractRepositoryWithElders.getByIndex(0);
+        Assert.assertTrue(contract.getContractOwner().getAge() > 25);
+
     }
 
     @Test
-    public void testSearchByPerson(){
-        ContractRepositoryImpl contractRepository=new ContractRepositoryImpl();
-        Person pupkin=new Person(1,"Vasya","Pupkin","Ivanovich", Gender.MALE,111,1111, java.sql.Date.valueOf("2000-04-09"));
-        MobileConnectionContract mobileConnectionContract=new MobileConnectionContract(444,0,88,1, java.sql.Date.valueOf("2020-03-09"), Date.valueOf("2023-04-09"),
-                77,pupkin);
-        DigitalTelevisionContract digitalTelevisionContract=new DigitalTelevisionContract("big",2, java.sql.Date.valueOf("2020-04-09"), java.sql.Date.valueOf("2022-04-09"),333,pupkin);
-        Person notPupkin=new Person(2,"Elizabeth","Peterson","Ivanovna",Gender.FEMALE,888,2222,Date.valueOf("1980-04-09"));
-        WireInternetContract wireInternetContract=new WireInternetContract(100,4,Date.valueOf("2018-03-09"),Date.valueOf("2024-08-09"),444,notPupkin);
-        contractRepository.add(mobileConnectionContract,digitalTelevisionContract,wireInternetContract);
-        Assert.assertArrayEquals(contractRepository.getByCriterias(e->e.getContractOwner().equals(pupkin)).toArray(),new Contract[]{mobileConnectionContract,digitalTelevisionContract});
+    public void testSearchByPerson() {
+        ContractRepositoryImpl contractRepository = new ContractRepositoryImpl();
+        Person pupkin = new Person(1, "Vasya", "Pupkin", "Ivanovich", Gender.MALE, 111, 1111, java.sql.Date.valueOf("2000-04-09"));
+        MobileConnectionContract mobileConnectionContract = new MobileConnectionContract(444, 0, 88, 1, java.sql.Date.valueOf("2020-03-09"), Date.valueOf("2023-04-09"),
+                77, pupkin);
+        DigitalTelevisionContract digitalTelevisionContract = new DigitalTelevisionContract("big", 2, java.sql.Date.valueOf("2020-04-09"), java.sql.Date.valueOf("2022-04-09"), 333, pupkin);
+        Person notPupkin = new Person(2, "Elizabeth", "Peterson", "Ivanovna", Gender.FEMALE, 888, 2222, Date.valueOf("1980-04-09"));
+        WireInternetContract wireInternetContract = new WireInternetContract(100, 4, Date.valueOf("2018-03-09"), Date.valueOf("2024-08-09"), 444, notPupkin);
+        contractRepository.add(mobileConnectionContract, digitalTelevisionContract, wireInternetContract);
+
+        ContractRepositoryImpl contractRepository1 = contractRepository.getByCriterias(e -> e.getContractOwner().equals(pupkin));
+        for (int i = 0; i < 2; i++) {
+            Contract contract = contractRepository1.getByIndex(i);
+            if (contract != null) {
+                Assert.assertTrue(contract.getContractOwner().getLastName().equals("Pupkin"));
+            }
+        }
     }
 
 
