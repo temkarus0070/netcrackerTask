ba7bb4fddf5e39466402037c13f0e4e9c8053842
artyom <pretkovartyom@gmail.com>
2021-11-09 22:39:41 +0300

    repair tests for sort and search function

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -25,34 +25,23 @@ public class SortTest {
         MobileConnectionContract mobileConnectionContract = new MobileConnectionContract(444, 0, 88, 1, java.sql.Date.valueOf("2020-03-09"), Date.valueOf("2023-04-09"),
                 77, pupkin);
         DigitalTelevisionContract digitalTelevisionContract = new DigitalTelevisionContract("big", 2, java.sql.Date.valueOf("2020-04-09"), java.sql.Date.valueOf("2022-04-09"), 333, pupkin);
-        Person notPupkin = new Person(2, "Elizabeth", "Peterson", "Ivanovna", Gender.FEMALE, 888, 2222, Date.valueOf("1980-04-09"));
-        WireInternetContract wireInternetContract = new WireInternetContract(100, 4, Date.valueOf("2018-03-09"), Date.valueOf("2024-08-09"), 444, notPupkin);
+        Person veryOldPerson = new Person(2, "Elizabeth", "Peterson", "Ivanovna", Gender.FEMALE, 888, 2222, Date.valueOf("1980-04-09"));
+        WireInternetContract wireInternetContract = new WireInternetContract(100, 4, Date.valueOf("2018-03-09"), Date.valueOf("2024-08-09"), 444, veryOldPerson);
         contractRepository.add(wireInternetContract, mobileConnectionContract, digitalTelevisionContract,youngestPersonMobileConnectionContract);
-        List<Contract> list = contractRepository.sort(new QuickSort(), new Comparator<Contract>() {
+
+        contractRepository.sort(new QuickSort(), new Comparator<Contract>() {
             @Override
             public int compare(Contract o1, Contract o2) {
-                if (o1.getContractOwner().getAge() > o2.getContractOwner().getAge())
+                if( o1.getContractOwner().getAge()>o2.getContractOwner().getAge())
                     return 1;
-                else if (o1.getContractOwner().getAge() == o2.getContractOwner().getAge())
+                else if( o1.getContractOwner().getAge()==o2.getContractOwner().getAge())
                     return 0;
-                return -1;
+                else return -1;
             }
         });
-        for(int i=1;i<list.size();i++){
-            Assert.assertTrue(list.get(i-1).getContractOwner().getAge()<=list.get(i).getContractOwner().getAge());
-        }
-        List<Contract> list1= contractRepository.sort(new BubbleSort(), new Comparator<Contract>() {
-            @Override
-            public int compare(Contract o1, Contract o2) {
-                if (o1.getContractOwner().getAge() > o2.getContractOwner().getAge())
-                    return -1;
-                else if (o1.getContractOwner().getAge() == o2.getContractOwner().getAge())
-                    return 0;
-                return 1;
-            }
-        });
-        for(int i=1;i<list1.size();i++){
-            Assert.assertTrue(list1.get(i-1).getContractOwner().getAge()>=list1.get(i).getContractOwner().getAge());
+
+        for(int i=1;i<4;i++){
+            Assert.assertTrue(contractRepository.getByIndex(i-1).getContractOwner().getAge()<=contractRepository.getByIndex(i).getContractOwner().getAge());
         }
     }
 }
