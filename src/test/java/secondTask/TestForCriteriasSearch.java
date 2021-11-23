bfbe0d373f5511c87ac5218704309fdb33317025
artyom <pretkovartyom@gmail.com>
2021-11-23 21:08:13 +0300

    add test to test more than one predicate at search

--- src/test/java/secondTask/TestForCriteriasSearch.java
+++ src/test/java/secondTask/TestForCriteriasSearch.java
@@ -52,5 +52,20 @@ public class TestForCriteriasSearch {
         }
     }
 
+    @Test
+    public void testSearchByPersonAndAge() {
+        ContractRepositoryImpl contractRepository = new ContractRepositoryImpl();
+        Person pupkin = new Person(1, "Vasya", "Pupkin", "Ivanovich", Gender.MALE, 111, 1111, java.sql.Date.valueOf("2000-04-09"));
+        MobileConnectionContract mobileConnectionContract = new MobileConnectionContract(444, 0, 88, 1, java.sql.Date.valueOf("2020-03-09"), Date.valueOf("2023-04-09"),
+                77, pupkin);
+        DigitalTelevisionContract digitalTelevisionContract = new DigitalTelevisionContract("big", 2, java.sql.Date.valueOf("2020-04-09"), java.sql.Date.valueOf("2022-04-09"), 333, pupkin);
+        Person notPupkin = new Person(2, "Elizabeth", "Peterson", "Ivanovna", Gender.FEMALE, 888, 2222, Date.valueOf("1980-04-09"));
+        WireInternetContract wireInternetContract = new WireInternetContract(100, 4, Date.valueOf("2018-03-09"), Date.valueOf("2024-08-09"), 444, notPupkin);
+        contractRepository.add(mobileConnectionContract, digitalTelevisionContract, wireInternetContract);
+
+        Repository<Contract,Long> contractRepository1 = contractRepository.getByCriterias(e -> e.getContractOwner().equals(pupkin),e->e.getContractOwner().getAge()>30);
+        Assert.assertNull(contractRepository1.getByIndex(0));
+    }
+
 
 }
