5a47be666f3c24dc4a18b071aded347a08840d56
temkarus0070 <pretkovartyom@gmail.com>
2021-12-08 14:02:55 +0300

    add ContractOwnerValidator and ContractValidator

---
+++ src/main/java/temkarus0070/firstTask/validation/validators/ContractOwnerValidator.java
@@ -0,0 +1,67 @@
+package temkarus0070.firstTask.validation.validators;
+
+import temkarus0070.firstTask.models.Person;
+import temkarus0070.firstTask.models.contract.Contract;
+import temkarus0070.firstTask.validation.Status;
+import temkarus0070.firstTask.validation.ValidationResult;
+import temkarus0070.firstTask.validation.Validator;
+
+import java.time.LocalDate;
+
+public class ContractOwnerValidator implements Validator<Contract> {
+    @Override
+    public ValidationResult validate(Contract contract) {
+        ValidationResult validationResult = new ValidationResult();
+        Person person = contract.getContractOwner();
+        if (person == null) {
+            validationResult.setStatus(Status.ERROR);
+            validationResult.setText("Contract owner is equal to null");
+            validationResult.setFirstErrorField("contractOwner");
+        } else if (person.getFirstName() == null) {
+            validationResult.setStatus(Status.ERROR);
+            validationResult.setText("Contract owner should have firstName");
+            validationResult.setFirstErrorField("contractOwner.firstName");
+        } else if (person.getLastName() == null) {
+            validationResult.setStatus(Status.ERROR);
+            validationResult.setText("Contract owner should have lastName");
+            validationResult.setFirstErrorField("contractOwner.lastName");
+        } else if (!validatePassportChapter(person)) {
+            validationResult.setStatus(Status.ERROR);
+            validationResult.setText("Contract owner passport should have correct passport chapter: more or equal to 1991 and less or equal to current Year");
+            validationResult.setFirstErrorField("contractOwner.passportChapter");
+        } else if (!validatePassportNum(person)) {
+            validationResult.setStatus(Status.ERROR);
+            validationResult.setText("Contract owner passport should have correct passport number consist of 6 digits");
+            validationResult.setFirstErrorField("contractOwner.passportNum");
+        } else if (!validatePersonBirthDate(person)) {
+            validationResult.setStatus(Status.ERROR);
+            validationResult.setText("Contract owner birth date should be before current date and shouldn't be null");
+            validationResult.setFirstErrorField("contractOwner.birthDate");
+        }
+        else if(person.getAge()<18){
+           validationResult.setStatus(Status.ERROR);
+            validationResult.setText("Contract owner should be older than 17");
+            validationResult.setFirstErrorField("contractOwner.birthDate");
+        }
+        return validationResult;
+    }
+
+    private boolean validatePassportChapter(Person person) {
+        return person.getPassportChapter() >= 1991 && person.getPassportChapter() <= LocalDate.now().getYear();
+    }
+
+    private boolean validatePassportNum(Person person) {
+        String str = String.valueOf(person.getPassportNum());
+        return str.length() == 6;
+    }
+
+    private boolean validatePersonBirthDate(Person person) {
+        boolean hasNullDate = person.getBirthDate() == null;
+        boolean isCorrectDate = true;
+        if (!hasNullDate) {
+            isCorrectDate = person.getBirthDate().compareTo(LocalDate.now()) < 0;
+        }
+        return hasNullDate && isCorrectDate;
+    }
+
+}
---
+++ src/main/java/temkarus0070/firstTask/validation/validators/ContractValidator.java
@@ -0,0 +1,29 @@
+package temkarus0070.firstTask.validation.validators;
+
+import temkarus0070.firstTask.models.Person;
+import temkarus0070.firstTask.models.contract.Contract;
+import temkarus0070.firstTask.validation.Status;
+import temkarus0070.firstTask.validation.ValidationResult;
+import temkarus0070.firstTask.validation.Validator;
+
+public class ContractValidator implements Validator<Contract> {
+    @Override
+    public ValidationResult validate(Contract contract) {
+        ValidationResult validationResult=new ValidationResult();
+        if(!checkContractDates(contract)) {
+            validationResult.setStatus(Status.ERROR);
+            validationResult.setFirstErrorField("beginDate || endDate");
+            validationResult.setText("beginDate or endDate equals to null or beginDate more than endDate");
+        }
+            return validationResult;
+    }
+
+    private boolean checkContractDates(Contract contract){
+        boolean haveNulls=contract.getBeginDate()==null || contract.getEndDate()==null;
+        boolean correctDates=true;
+        if(!haveNulls)
+            correctDates=contract.getBeginDate().compareTo(contract.getEndDate())<=0;
+        return haveNulls&&correctDates;
+    }
+
+}
